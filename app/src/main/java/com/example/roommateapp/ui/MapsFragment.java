7991588fefb0724169c9fb596f6a0d7312bd1d54
Moses gaines <gaines.215@osu.edu>
2024-04-01 16:01:35 -0400

    Added buttons to maps fragment and took gps button away from evrything except for list fragment

--- app/src/main/java/com/example/roommateapp/ui/MapsFragment.java
+++ app/src/main/java/com/example/roommateapp/ui/MapsFragment.java
@@ -5,14 +5,20 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.roommateapp.R;
+import com.example.roommateapp.databinding.ListFragmentBinding;
+import com.example.roommateapp.databinding.MapsFragmentBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,8 +27,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,77 +39,72 @@ import com.google.android.gms.tasks.Task;
  */
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
-    private int CHECK_MAPS_PERMISSIONS = 1;
+    private MapsFragmentBinding binding;
     private static final int REQUEST_CODE = 101;
-    private GoogleMap mMap;
     Location currentLocation;
     FusedLocationProviderClient fusedLocationProviderClient;
+    private FirebaseAuth mAuth;
+
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
-        getCurrentLocation();
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.maps_fragment, container, false);
-        return view;
+//        View view = inflater.inflate(R.layout.maps_fragment, container, false);
+
+        binding = MapsFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
+        getCurrentLocation();
+
+        binding.listsButton.setOnClickListener(e -> NavHostFragment.findNavController(MapsFragment.this).navigate(R.id.action_MapsFragment_to_ListFragment));
+        binding.signoutButton.setOnClickListener(e -> {
+
+            signOut();
+
+            NavHostFragment.findNavController(MapsFragment.this)
+                    .navigate(R.id.action_MapsFragment_to_LoginFragment);
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
     }
 
     private void getCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            Toast.makeText(this.getContext(), "If you deny, you will not be able to see your current location on the map.", Toast.LENGTH_SHORT).show();
             return;
         }
         Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
-        locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    currentLocation = location;
 
-                    // Initialize map fragment
-                    SupportMapFragment supportMapFragment = (SupportMapFragment)
-                            getChildFragmentManager().findFragmentById(R.id.google_map);
-                    assert supportMapFragment != null;
-                    supportMapFragment.getMapAsync(MapsFragment.this);
-                    // Async map
-//                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-//                        @Override
-//                        public void onMapReady(GoogleMap googleMap) {
-//                            // When map is loaded
-//                            LatLng currLocation = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-//                            googleMap.addMarker(new MarkerOptions().position(currLocation).title("My Location"));
-//                            googleMap.animateCamera(CameraUpdateFactory.newLatLng(currLocation));
-//                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currLocation, 10));
-////                            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-////                                @Override
-////                                public void onMapClick(LatLng latLng) {
-////                                    // When clicked on map
-////                                    // Initialize marker options
-////                                    MarkerOptions markerOptions = new MarkerOptions();
-////                                    // Set position of marker
-////                                    markerOptions.position(latLng);
-////                                    // Set title of marker
-////                                    markerOptions.title(latLng.latitude + " : " + latLng.longitude);
-////                                    // Remove all marker
-////                                    googleMap.clear();
-////                                    // Animating to zoom the marker
-////                                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-////                                    // Add marker on map
-////                                    googleMap.addMarker(markerOptions);
-////                                }
-////                            });
-//                        }
-//                    });
-                }
+        locationTask.addOnCompleteListener(task -> locationTask.addOnSuccessListener(location -> {
+            if (location != null) {
+                currentLocation = location;
+
+                // Initialize map fragment
+                SupportMapFragment supportMapFragment = (SupportMapFragment)
+                        getChildFragmentManager().findFragmentById(R.id.google_map);
+                assert supportMapFragment != null;
+                supportMapFragment.getMapAsync(MapsFragment.this);
             }
-        });
+        }));
     }
 
 //    @Override
@@ -116,7 +119,11 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 //        }
 //    }
 
-
+    private void signOut() {
+        mAuth.signOut();
+        Toast.makeText(getContext(), "Successful Sign Out.",
+                Toast.LENGTH_SHORT).show();
+    }
 
 
     @Override
